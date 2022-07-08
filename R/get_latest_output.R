f45ed033116054b4ac5c4c6d01ec8e94e992b816
tomwilsonsco <thomaswilson81@gmail.com>
2022-07-08 09:49:41 +0100

    add file prefix argument for alternative outputs

--- R/get_latest_output.R
+++ R/get_latest_output.R
@@ -1,6 +1,7 @@
-get_latest_output <- function(folder_name, file_type = "csv") {
+get_latest_output <- function(folder_name, file_type = "csv", file_prefix="shiny_app_usage") {
   file_pattern <- paste0("*.", file_type)
   all_files <- list.files(folder_name, pattern = file_pattern)
-  latest_file <- sort(all_files, decreasing = TRUE)[1]
+  filtered_files <- all_files[grepl(file_prefix, all_files)]
+  latest_file <- sort(filtered_files, decreasing = TRUE)[1]
   file.path(folder_name, latest_file)
 }
