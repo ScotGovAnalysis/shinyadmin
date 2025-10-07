740c730439c5fcbcddc3569611b3aa6396ddadd8
alice-hannah <alice.hannah@gov.scot>
2025-10-07 15:17:01 +0100

    Add setup script

---
+++ scripts/00_setup.R
@@ -0,0 +1,38 @@
+# Name: 00_setup.R
+# Description: Load packages and functions required for data processing
+
+
+# 1 - Load packages ----
+
+library(rsconnect)
+library(dplyr)
+library(lubridate)
+library(stringr)
+library(here)
+library(readr)
+library(here)
+library(yaml)
+library(purrr)
+library(tidyr)
+library(lubridate)
+library(RtoSQLServer)
+
+
+# 2 - Load functions ----
+
+walk(
+  list.files(here("R"), pattern = "*.R$", full.names = TRUE),
+  source
+)
+
+
+# 3 - Set config parameters ----
+
+config <- c(
+  read_yaml(here("config.yml")),
+  db = "AdministrationShinyapps",
+  schema = "supportadmin"
+)
+
+
+### END OF SCRIPT ###
