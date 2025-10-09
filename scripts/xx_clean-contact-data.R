3b9c6251278b52fc70c619a0d4a0236850a65773
alice-hannah <alice.hannah@gov.scot>
2025-10-09 17:36:48 +0100

    Add script to clean contact data from new MS Form

---
+++ scripts/xx_clean-contact-data.R
@@ -0,0 +1,9 @@
+# Name: xx_clean-contact-data.R
+# Description: Clean contact data from MS Form and save to ADM
+
+# 0 - Run setup script ----
+
+source(here::here("scripts", "00_setup.R"))
+
+
+### END OF SCRIPT ###
