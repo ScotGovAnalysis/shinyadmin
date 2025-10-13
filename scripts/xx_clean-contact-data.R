ea5011ab475b47c646584d1ca830048eca0c9b1d
alice-hannah <alice.hannah@gov.scot>
2025-10-13 16:26:43 +0100

    Remove cols, set var types and save to ADM

--- scripts/xx_clean-contact-data.R
+++ scripts/xx_clean-contact-data.R
@@ -1,6 +1,7 @@
 # Name: xx_clean-contact-data.R
 # Description: Clean contact data from MS Form and save to ADM
 
+
 # 0 - Run setup script ----
 
 source(here::here("scripts", "00_setup.R"))
@@ -10,7 +11,21 @@ source(here::here("scripts", "00_setup.R"))
 
 contacts <- 
   read_xlsx(config$ms_form_path) %>%
-  rename_cols(lookups$ms_form_names)
+  rename_cols(lookups$ms_form_names) %>%
+  select(-starts_with("ms")) %>%
+  mutate(completed_time = ymd_hms(completed_time),
+         across(!matches("time"), as.character))
+
+
+# 2 - Write data to ADM ----
+
+write_dataframe_to_db(
+  config$adm_server,
+  config$database,
+  config$schema,
+  table_name = "contacts_new",
+  dataframe = contacts
+)
 
 
 ### END OF SCRIPT ###
