11d490df93558929c203e5cec916061ece4c7573
alice-hannah <alice.hannah@gov.scot>
2025-11-10 16:54:38 +0000

    Refine ms_form_names docs

--- R/data.R
+++ R/data.R
@@ -1,8 +1,12 @@
 #' Microsoft Form Names
 #'
 #' A lookup dataset for renaming columns in the Microsoft Forms contact dataset.
-#' Contains two columns: `new` and `old`. The `old` column should match the
-#' question wording in the MS form exactly. The `new` column contains clean
-#' snakecase names to use in the analysis dataset.
+#'
+#' @format
+#' A tibble with 2 columns:
+#' \describe{
+#'   \item{new}{Clean snakecase names to use in the analysis dataset}
+#'   \item{old}{Exact question wording in the MS form}
+#' }
 
 "ms_form_names"
