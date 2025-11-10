220d848e7f34342f75052b4f0b5ac9241003ca28
alice-hannah <alice.hannah@gov.scot>
2025-11-10 15:58:01 +0000

    Add ms_form_names as pkg dataset

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Microsoft Form Names
+#'
+#' A lookup dataset for renaming columns in the Microsoft Forms contact dataset.
+#' Contains two columns: `new` and `old`. The `old` column should match the
+#' question wording in the MS form exactly. The `new` column contains clean
+#' snakecase names to use in the analysis dataset.
+
+"ms_form_names"
